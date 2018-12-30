ce2f1755082ba3ac7ea2ca64d03f959e04df04c7
Adrien Destugues <pulkomandy@pulkomandy.tk>
2018-12-30 20:00:10 +0100

    Haiku: implement help support

--- src/haiku/iuphaiku_help.c
+++ src/haiku/iuphaiku_help.c
@@ -12,12 +12,57 @@
 
 #include "iup_str.h"
 
-#define UNIMPLEMENTED printf("%s (%s %d) UNIMPLEMENTED\n",__func__,__FILE__,__LINE__);
+int IupExecute(const char *filename, const char* parameters)
+{
+  int ret;
+  if (parameters)
+  {
+    char* cmd = (char*)malloc(sizeof(char)*(strlen(filename) + strlen(parameters) + 4));
+    sprintf(cmd, "%s %s &", filename, parameters);
+	fprintf(stderr, "%s(%s)\n", __func__, cmd);
+    ret = system(cmd);
+    free(cmd);
+  }
+  else
+  {
+    char* cmd = (char*)malloc(sizeof(char)*(strlen(filename) + 3));
+    sprintf(cmd, "%s &", filename);
+    ret = system(cmd);
+    free(cmd);
+  }
+  if (ret == -1)
+    return -1;
+  return 1;
+}
+
+int IupExecuteWait(const char *filename, const char* parameters)
+{
+  int ret;
+  if (parameters)
+  {
+    char* cmd = (char*)malloc(sizeof(char)*(strlen(filename) + strlen(parameters) + 3));
+    sprintf(cmd, "%s %s", filename, parameters);
+    ret = system(cmd);
+    free(cmd);
+  }
+  else
+    ret = system(filename);
+  if (ret == -1)
+    return -1;
+  return 1;
+}
 
 int IupHelp(const char *url)
 {
-  // TODO open the html page in web browser
-  UNIMPLEMENTED
+  char *browser = getenv("IUP_HELPAPP");
+  if (!browser) 
+    browser = IupGetGlobal("HELPAPP");
 
-  return -1;
+  // TODO use mime db directly?
+  if (!browser) 
+  { 
+	  browser = "open";
+  }
+  
+  return IupExecute(browser, url);
 }
